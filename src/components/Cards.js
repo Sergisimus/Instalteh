4d3a339a21bbb820d71146ccf34c51f0d243f831
Sergisimus <mateo.sergo123@gmail.com>
2024-02-25 22:35:05 +0000

    fixed some css

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,7 +5,10 @@ import CardItem from './CardItem';
 function Cards() {
   return (
     <div className='cards'>
-      <h1>Check out these EPIC Destinations!</h1>
+      <h1>About Us</h1>
+      <div className='about_us_container'>
+        Mi smo tim inženjera iz Rijeke koji iza sebe imaju mnoge projekte.
+      </div>
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,24 +6,27 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src='videos/video-1.mp4' autoPlay loop muted />
-      <h1>ADVENTURE AWAITS</h1>
+      <video poster='images/img-1.jpg' autoPlay loop muted playsInline>
+        <source src='videos/video-1.mp4' type='video/mp4'/>     
+        Not supported
+      </video>
+      <h1>INSTALTEH</h1>
       <p>What are you waiting for?</p>
       <div className='hero-btns'>
-        <Button
+        {/* <Button
           className='btns'
           buttonStyle='btn--outline'
           buttonSize='btn--large'
         >
           GET STARTED
-        </Button>
-        <Button
+        </Button> */}
+        {/* <Button
           className='btns'
           buttonStyle='btn--primary'
           buttonSize='btn--large'
         >
-          WATCH TRAILER <i className='far fa-play-circle' />
-        </Button>
+          WATCH TRAILER
+        </Button> */}
       </div>
     </div>
   );
