829781eb6db09d72deabe6310f5e31b8b7be469c
Sergisimus <mateo.sergo123@gmail.com>
2024-03-01 20:54:01 +0000

    form styles

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -6,16 +6,16 @@ function Contact() {
   return (
     <>
       <div className='contact'>
-        <h1>Contact Us</h1>
+        <h3>Contact Us</h3>
         <form className='contact_form'>
           <div className='first_row'>
             <div className='first_row_item'>
               <label className='contact_name contact_item'>NAME</label>
-              <input className='contact_name_input contact_input' name='name' type='text' placeholder='Your Name'></input>
+              <input className='contact_name_input contact_input' name='name' type='text' placeholder='Name'></input>
             </div>
             <div className='first_row_item'> 
               <label className='contact_email contact_item'>EMAIL</label> 
-              <input className='contact_email_input contact_input' name='email' type='email' placeholder='Your Email'></input>
+              <input className='contact_email_input contact_input' name='email' type='email' placeholder='Email'></input>
             </div>           
           </div>          
           <label className='contact_subject contact_item'>SUBJECT</label> 
